d8ac3b74eebd665ff2d03fe7d55f996c9d42e062
YujieSang <34143747+YujieSang@users.noreply.github.com>
2025-07-23 21:49:11 +0800

    Update 1d_Ising_quantum.py

--- 1d_Ising_quantum.py
+++ 1d_Ising_quantum.py
@@ -1,18 +1,21 @@
-
-from qiskit import QuantumCircuit
-from qiskit_aer import AerSimulator
-import matplotlib.pyplot as plt
+from qiskit_ibm_runtime import QiskitRuntimeService, SamplerV2 as Sampler
+from qiskit_aer import Aer
+from qiskit import QuantumCircuit, transpile
+from qiskit.transpiler import generate_preset_pass_manager
 from qiskit.visualization import plot_histogram
+import numpy as np
+import matplotlib.pyplot as plt 
 
+# Logical circuit
 def Ising_1D(N, J, g, dt, r):
 
     qc = QuantumCircuit(N, N)
-    # init state
+    # H
     for i in range(N):
         qc.h(i)
     qc.barrier()
     
-    # Trotterization
+    # Trotterization of time evo operator
     for _ in range(r):
         # ZZ
         for i in range(N - 1):
@@ -28,7 +31,7 @@ def Ising_1D(N, J, g, dt, r):
     return qc
 
 # parameters
-N = 10           # number of spins/qubits
+N = 5           # number of spins/qubits
 J = 1.0         
 g = 0.5         
 t_total = 1.0   # total time
@@ -36,14 +39,36 @@ r = 4           # # of Trotter slices
 dt = t_total / r
 
 qc = Ising_1D(N, J, g, dt, r)
-
-# Visual 
 fig = qc.draw(output='mpl', fold=100)
-sim = AerSimulator()
-job = sim.run(qc, shots=1024)
-counts = job.result().get_counts()
-print("Measurement counts:", counts)
-plt.figure(figsize=(6,4))
-plot_histogram(counts)
-plt.title("Outcome Distribution")
+# Local simulator
+sim_counts = Aer.get_backend("aer_simulator").run(
+    transpile(qc, Aer.get_backend("aer_simulator")), shots=1024
+).result().get_counts()
+
+# Choose backend
+service = QiskitRuntimeService()
+backend = service.backend("ibm_rensselaer")
+
+# ISA circuit
+isa_circuit = generate_preset_pass_manager(
+    optimization_level=2, backend=backend
+).run(qc)
+
+
+print()
+
+
+# run
+sampler = Sampler(backend)
+opts = sampler.options
+opts.dynamical_decoupling.enable = True  # suppress decoherence
+opts.twirling.enable_gates = True 
+job = sampler.run([isa_circuit], shots=1024)
+result = job.result()
+counts_hw = result[0].data.c.get_counts()
+
+# Visualise
+plot_histogram([sim_counts, counts_hw],
+               legend=["Simulator", backend.name],
+               sort="desc")
 plt.show()
